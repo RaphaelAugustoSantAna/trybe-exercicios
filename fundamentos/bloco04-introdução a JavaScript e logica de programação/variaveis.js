6f5791de1c0c3a95d0f2571e94e9bd53b97f9d7c
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-06 18:14:14 -0300

    adicionando valores aritimeticos em variaveis e constantes

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/variaveis.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/variaveis.js
@@ -1,13 +1,4 @@
-let patientId = '50';
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
-
-console.log (typeof patientId);
-console.log (typeof isEnrolled);
-console.log (typeof patientInfo);
-console.log (typeof patientEmail);
-console.log (typeof patientAge)
+const base = 5;
+let height = 8;
+console.log(base*height);
+const area = 40;
