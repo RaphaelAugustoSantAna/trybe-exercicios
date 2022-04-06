9ee5d35dea301807d3671f026c3aae612ee89bd4
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-06 16:15:20 -0300

    adicionando tipos primitivos

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/variaveis.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/variaveis.js
@@ -1,7 +1,13 @@
-const myName = "Raphael";
-const birthCity = "São Paulo";
-"barueri"
-const birthYear = "2030";
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log (typeof patientId);
+console.log (typeof isEnrolled);
+console.log (typeof patientInfo);
+console.log (typeof patientEmail);
+console.log (typeof patientAge)
