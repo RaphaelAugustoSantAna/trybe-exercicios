446ea5b99fe6315da75464e7ef5af2719f7447ab
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-07 18:45:14 -0300

    exercicio 4 imprime a mensagem maior ou menor que 20

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
@@ -1,9 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let resultado = 0;
 for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
+    resultado += numbers[index];
 }
 
-let media = soma /=numbers.length;
+let media = resultado /= numbers.length;
+if(resultado > 20){
+    console.log('Valor maior que vinte');
+    
+} else {
+    console.log('Valor menor ou igual a 20');
 
-console.log(media);
+}
