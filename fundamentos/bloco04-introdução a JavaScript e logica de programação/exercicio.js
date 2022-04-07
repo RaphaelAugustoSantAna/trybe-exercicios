c6efea97b57d999765e64fbd9bdbfb2b8ee0b323
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-07 09:39:53 -0300

    adicionando exercicio 1

---
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/exercicio.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 5;
+console.log('adição:'+(a+b));
+console.log('subtração:'+(a-b));
+console.log('multiplicação:'+(a*b));
+console.log('divisão:'+(a/b));
+console.log('módulo'+(a%b));
