1456dfa0e0e472e248d567fb8fdb8417209c5570
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-07 19:29:04 -0300

    exercicio 7 menor valor do array

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
@@ -1,14 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let menorNumero = numbers [0];
 for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0 ) {
-        resultado += 1;
+    if (numbers[index] < menorNumero ) {
+        menorNumero = numbers[index];
     }
 }
-if( resultado === 0 ){
-    console.log('nenhum valor impar encontrado')
-
-    }else{
-        console.log(resultado);
-
-    }
+console.log(menorNumero);
