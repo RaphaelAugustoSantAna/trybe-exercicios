98bb24e98d34705b4759d46597274d998dec85f9
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-07 19:06:53 -0300

    adicionando exercicio 5 descubra o maior valor do array

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
@@ -1,14 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
+let maiorValor = numbers[0];
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorValor) {
+        maiorValor = numbers[index];
+    }
 }
-
-let media = resultado /= numbers.length;
-if(resultado > 20){
-    console.log('Valor maior que vinte');
-    
-} else {
-    console.log('Valor menor ou igual a 20');
-
-}
+console.log(maiorValor);
