d138a9ff1ba9f7e9825d856140b1590ec5408b55
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-07 09:53:49 -0300

    adicionando exercicio 3

---
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/exercicio3.js
@@ -0,0 +1,12 @@
+const a = 8;
+const b = 6;
+const c = 4;
+if(a > b && a > c ){
+    console.log('o maior número é :'+ a + '(a)');
+
+} else if( b > a && b > c ){
+    console.log('o maior número é :'+ b + '(b)');
+
+}else{
+    console.log('o maior número é :'+ c + '(c)');
+}
