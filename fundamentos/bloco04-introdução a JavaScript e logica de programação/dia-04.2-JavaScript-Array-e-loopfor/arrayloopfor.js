59cefd369c775985631fa5bd366b1eddf5570c5f
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-08 12:46:24 -0300

    adiciona exercicio 9 divisão do elemento do array por 2

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.2-JavaScript-Array-e-loopfor/arrayloopfor.js
@@ -1,5 +1,11 @@
-const numeros = [];
-for(let index = 1; index <= 25; index += 1){
-   numeros.push(index);
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
 }
- console.log(numeros);
+
+console.log(numbers);
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2);
+  };
