2cd470ddcddc67be66362675856aef011c5df9e9
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-11 10:25:42 -0300

    adiciona exercicio bonus quadrado e triangulo retangulo

---
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.3-logica-de-programação-e-algoritimo/bonus.js
@@ -0,0 +1,18 @@
+// let n = 5;
+// let asterisco = '*';
+// let espaco = '';
+
+// for(let index = 0; index < n; index += 1){
+// espaco = espaco + asterisco;
+// }
+// for(let index = 0;index < n; index += 1){
+// console.log(espaco);
+// }
+let trianguloRetangulo = 5;
+let asterisco = '*';
+let espaco = '';
+
+for(let index = 0; index <= trianguloRetangulo; index += 1){
+console.log(espaco);
+espaco = espaco + asterisco;
+}
