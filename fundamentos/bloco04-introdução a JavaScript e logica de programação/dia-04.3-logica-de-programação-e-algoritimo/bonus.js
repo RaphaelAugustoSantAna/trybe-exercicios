8dd78c1c13d705eda0fed97533074b9ef890c408
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-11 10:35:46 -0300

    adiciona exercio 3 inverter piramide

--- fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.3-logica-de-programação-e-algoritimo/bonus.js
+++ fundamentos/bloco04-introdução a JavaScript e logica de programação/dia-04.3-logica-de-programação-e-algoritimo/bonus.js
@@ -8,11 +8,12 @@
 // for(let index = 0;index < n; index += 1){
 // console.log(espaco);
 // }
-let trianguloRetangulo = 5;
-let asterisco = '*';
-let espaco = '';
+// let trianguloRetangulo = 5;
+// let asterisco = '*';
+// let espaco = '';
 
-for(let index = 0; index <= trianguloRetangulo; index += 1){
-console.log(espaco);
-espaco = espaco + asterisco;
-}
+// for(let index = 0; index <= trianguloRetangulo; index += 1){
+// console.log(espaco);
+// espaco = espaco + asterisco;
+// }
+// 
