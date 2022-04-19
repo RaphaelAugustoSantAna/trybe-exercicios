622ac9b76814c73b655eff262c11b357f03997f6
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-19 10:36:01 -0300

    exercicio 4 botão sexta feira

--- fundamentos/bloco05-JavaScript:DOM, eventos e web storage/exercicios-5.3/script.js
+++ fundamentos/bloco05-JavaScript:DOM, eventos e web storage/exercicios-5.3/script.js
@@ -1,78 +1,100 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
-    for (let index = 0; index < weekDays.length; index += 1) {
-        const days = weekDays[index];
-        const dayListItem = document.createElement('li');
-        dayListItem.innerHTML = days;
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
 
-        weekDaysList.appendChild(dayListItem);
-    };
-};
+    weekDaysList.appendChild(dayListItem);
+  }
+}
 
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 // exercicio 1
-let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
 function diasDoMes() {
-    let listaDeDias = document.querySelector('#days');
+  let listaDeDias = document.querySelector("#days");
 
-    for (let index = 0; index < dezDaysList.length; index += 1) {
-        let dia = dezDaysList[index];
-        let itemDia = document.createElement('li');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let dia = dezDaysList[index];
+    let itemDia = document.createElement("li");
 
-        if (dia === 24 | dia === 31) {
-            itemDia.className = 'dia feriado';
-            itemDia.innerHTML = dia;
-            listaDeDias.appendChild(itemDia);
-        } else if (dia === 4 | dia === 11 | dia === 18) {
-            itemDia.className = 'dia sexta-feira';
-            itemDia.innerHTML = dia;
-            listaDeDias.appendChild(itemDia);
-        } else if (dia === 25) {
-            itemDia.className = 'dia feriado sexta-feira';
-            itemDia.innerHTML = dia;
-            listaDeDias.appendChild(itemDia);
-        } else {
-            itemDia.innerHTML = dia;
-            itemDia.className = 'dia';
-            listaDeDias.appendChild(itemDia);
-        }
-    };
-};//exercicio 2
+    if ((dia === 24) | (dia === 31)) {
+      itemDia.className = "dia feriado";
+      itemDia.innerHTML = dia;
+      listaDeDias.appendChild(itemDia);
+    } else if ((dia === 4) | (dia === 11) | (dia === 18)) {
+      itemDia.className = "dia sexta-feira";
+      itemDia.innerHTML = dia;
+      listaDeDias.appendChild(itemDia);
+    } else if (dia === 25) {
+      itemDia.className = "dia feriado sexta-feira";
+      itemDia.innerHTML = dia;
+      listaDeDias.appendChild(itemDia);
+    } else {
+      itemDia.innerHTML = dia;
+      itemDia.className = "dia";
+      listaDeDias.appendChild(itemDia);
+    }
+  }
+} //exercicio 2
 diasDoMes();
 
 function botaoFeriado(nomeDoBotao) {
-let caixaBotao = document.querySelector('.buttons-container');
-let novoBotao = document.createElement('button');
-let novoBotaoID = 'btn-feriado';
+  let caixaBotao = document.querySelector(".buttons-container");
+  let novoBotao = document.createElement("button");
+  let novoBotaoID = "btn-feriado";
 
-novoBotao.innerHTML = nomeDoBotao;
-novoBotao.id = novoBotaoID;
-caixaBotao.appendChild(novoBotao);
-};
-botaoFeriado('feriados');
+  novoBotao.innerHTML = nomeDoBotao;
+  novoBotao.id = novoBotaoID;
+  caixaBotao.appendChild(novoBotao);
+}
+botaoFeriado("feriados");
 
 //exercicio 3
-function displayFeriado(){
- let feriadoBotao = document.querySelector('#btn-feriado');
- let feriadao = document.querySelectorAll('.feriado');
- let corDeFundo = 'rgb(238,238,238)';
- let novaCor = 'white';
+function displayFeriado() {
+  let feriadoBotao = document.querySelector("#btn-feriado");
+  let feriadao = document.querySelectorAll(".feriado");
+  let corDeFundo = "rgb(238,238,238)";
+  let novaCor = "white";
 
- feriadoBotao.addEventListener('click',function(){
-     for(let index = 0; index < feriadao.length; index += 1){
-        if(feriadao[index].style.corDeFundo === novaCor){
-         feriadao[index].style.corDeFundo = corDeFundo;
-        }else{
-            feriadao[index].style.corDeFundo = novaCor;
-        }
-     }
- })
-};
+  feriadoBotao.addEventListener("click", function () {
+    for (let index = 0; index < feriadao.length; index += 1) {
+      if (feriadao[index].style.corDeFundo === novaCor) {
+        feriadao[index].style.corDeFundo = corDeFundo;
+      } else {
+        feriadao[index].style.corDeFundo = novaCor;
+      }
+    }
+  });
+}
 displayFeriado();
 
-//exercicio 4 
+//exercicio 4
+function botaoSexta(buttonName) {
+    let botaoContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-fryday';
+
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    botaoContainer.appendChild(newButton);
+
+};
+botaoSexta('Sexta-feira');
