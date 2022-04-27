36fe65a8b72ee04d121069dd93595d93964bb73f
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-04-27 11:12:38 -0300

    adiciona exercicio trybe trip

--- fundamentos/bloco06-HTML, e CSS:forms, flexbox e responsivo/6.1: HTML & CSS - Forms/script.js
+++ fundamentos/bloco06-HTML, e CSS:forms, flexbox e responsivo/6.1: HTML & CSS - Forms/script.js
@@ -1,18 +1,18 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+//  SELETORES
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+// const HREF_LINK = document.querySelector("#href");
 
-HREF_LINK.addEventListener('click', (event) =>{
-event.preventDefault();
-});
+// HREF_LINK.addEventListener('click', (event) =>{
+// event.preventDefault();
+// });
 
-INPUT_CHECKBOX.addEventListener('click', (event) =>{
-    event.preventDefault();
-    });
+// INPUT_CHECKBOX.addEventListener('click', (event) =>{
+//     event.preventDefault();
+//     });
 
-INPUT_TEXT.addEventListener('keypress', (event) =>{
-    const letra = event.key;
-    if(letra !== 'a')
-        event.preventDefault();
-        });
+// INPUT_TEXT.addEventListener('keypress', (event) =>{
+//     const letra = event.key;
+//     if(letra !== 'a')
+//         event.preventDefault();
+//         });
