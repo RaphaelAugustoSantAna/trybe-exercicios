a1619a167520771f6967a45ef66803174d64c14d
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-05-13 23:42:11 -0300

    exercicio - 2 resultado do sorteio

--- fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.1: Introdução a Higher Order Functions/script.js
+++ fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.1: Introdução a Higher Order Functions/script.js
@@ -1,20 +1,29 @@
-const cadastro = (string) =>{
-    const novoCadastro = string.replace(/ /g, '_');
-    const email = `${novoCadastro}@trybe.com`;
-    const objeto = {
-        name:string,
-        email:email,
-    }
-    return objeto;
-}
+// const cadastro = (string) =>{
+//     const novoCadastro = string.replace(/ /g, '_');
+//     const email = `${novoCadastro}@trybe.com`;
+//     const objeto = {
+//         name:string,
+//         email:email,
+//     }
+//     return objeto;
+// }
 
-const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('pedro guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback('luiza drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback('carla paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
-  const objeto = newEmployees(cadastro);
-  console.log(objeto);
+// const newEmployees = (callback) => {
+//     const employees = {
+//       id1: callback('pedro guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: callback('luiza drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: callback('carla paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
+//   const objeto = newEmployees(cadastro);
+//   console.log(objeto);
+
+const numAposta = (aposta, sorteio) => aposta === sorteio;
+
+const resSorteio = (aposta, callback) => {
+    const sorteio = Math.floor((Math.random()*5) + 1);
+    return callback(aposta, sorteio) ? 'Parabéns você ganhou!' : 'Tente novamente.';
+};
+
+console.log(resSorteio(1,numAposta));
