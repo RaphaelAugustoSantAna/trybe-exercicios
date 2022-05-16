b0b4c0bbb0a71231b9322b5b3dc65208a2fcacc2
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-05-16 09:36:55 -0300

    exercicio de fixação HOF

--- fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.1: Introdução a Higher Order Functions/script.js
+++ fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.1: Introdução a Higher Order Functions/script.js
@@ -1,3 +1,5 @@
+// exercicio - 1
+
 // const cadastro = (string) =>{
 //     const novoCadastro = string.replace(/ /g, '_');
 //     const email = `${novoCadastro}@trybe.com`;
@@ -19,11 +21,40 @@
 //   const objeto = newEmployees(cadastro);
 //   console.log(objeto);
 
-const numAposta = (aposta, sorteio) => aposta === sorteio;
 
-const resSorteio = (aposta, callback) => {
-    const sorteio = Math.floor((Math.random()*5) + 1);
-    return callback(aposta, sorteio) ? 'Parabéns você ganhou!' : 'Tente novamente.';
+// ------------------------------------------------------------
+// exercicio - 2
+
+// const numAposta = (aposta, sorteio) => aposta === sorteio;
+
+// const resSorteio = (aposta, callback) => {
+//     const sorteio = Math.floor((Math.random()*5) + 1);
+//     return callback(aposta, sorteio) ? 'Parabéns você ganhou!' : 'Tente novamente.';
+// };
+
+// console.log(resSorteio(1,numAposta));
+
+//------------------------------------------------------------
+//exercicio - 3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const notaDoAluno = (rightAnswers, studentAnsers) => {
+    if(rightAnswers === studentAnsers){
+        return 1;
+    }if(studentAnsers === 'N/A'){
+        return 0;
+    }
+    return -0,5;
 };
 
-console.log(resSorteio(1,numAposta));
+const contador = (rightAnswers, studentAnsers, action) =>{
+    let cont = 0;
+    for (let index = 0; index < rightAnswers.length; index += 1){
+        const actionReturn = action(rightAnswers[index], studentAnsers[index]);
+        contador + actionReturn;
+    }
+    return `Resultado final: ${contador} pontos.`
+};
+console.log(contador(RIGHT_ANSWERS,STUDENT_ANSWERS,notaDoAluno));
