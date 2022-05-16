cfb86273e2829c3524ae6ae5fec613826025f440
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-05-16 19:08:42 -0300

    exercicio .find

--- fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.2: Higher Order Functions - forEach, find, some, every, sort/script.js
+++ fundamentos/bloco08-higherorderfunctions do JavaScript ES6/Dia 8.2: Higher Order Functions - forEach, find, some, every, sort/script.js
@@ -1,13 +1,49 @@
-const emailListInData = [
-    'roberta@email.com',
-    'paulo@email.com',
-    'anaroberta@email.com',
-    'fabiano@email.com',
-  ];
+// .forEach
+
+// const emailListInData = [
+//     'roberta@email.com',
+//     'paulo@email.com',
+//     'anaroberta@email.com',
+//     'fabiano@email.com',
+//   ];
   
-  const showEmailList = (email) => {
-    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-  };
+//   const showEmailList = (email) => {
+//     console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+//   };
   
-  // Adicione seu código aqui
-  emailListInData.forEach(showEmailList);
+//   // Adicione seu código aqui
+//   emailListInData.forEach(showEmailList);
+
+// .find
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = () => {
+// return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+// }
+
+// console.log(findDivisibleBy3And5())
+
+//---------------------------------------------
+
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = () => {
+//   return names.find((name) => name.length === 5)
+// }
+
+// console.log(findNameWithFiveLetters());
+
+//------------------------------------------------
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    return musicas.find((musica) => musica.id === id);
+  }
+  
+  console.log(findMusic('31031685'))
