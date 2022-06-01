de292aae43c3b9be1d37bccca9c65ba549e3b33e
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-06-01 10:12:20 -0300

    exercicio - 2

--- front end/bloco 9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
+++ front end/bloco 9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
@@ -1,32 +1,73 @@
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+// Exercicio - 1
 
-const mars = {
-  name: "Mars",
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: "kilometers",
-  },
-};
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-const venus = {
-  name: "Venus",
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: "kilometers",
-  },
-};
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-const jupiter = {
-  name: "Jupiter",
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: "kilometers",
-  },
-};
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// console.log(planetDistanceFromSun(venus)); // B
+// console.log(planetDistanceFromSun(jupiter)); // C
 
 // A ordem de finalização é A, B e C.
+
+//----------------------------------------------------------------------------------------
+
+//exercicio - 2
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+//  A ordem de finalização é A, C e B.
+
+//-----------------------------------------------------------------
