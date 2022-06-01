059faf2b63a89a379044c285fc4a34d48ae2136d
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-06-01 10:25:35 -0300

    exercicio - 4

--- front end/bloco 9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
+++ front end/bloco 9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
@@ -86,3 +86,23 @@
 //   };
   
 //   getPlanet(); // imprime Marte depois de 4 segundos
+
+//----------------------------------------------------------------------------
+
+//Exercicio - 4
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () =>{
+    const temperaturaAtual = getMarsTemperature();
+    setTimeout(() => console.log(`Mars temperature is: ${temperaturaAtual} degree Celsius`), messageDelay());
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
