70a1f3d5c7e8e75932a8fc2705e67b6314d1770a
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-06-10 18:55:48 -0300

    exercicios com react

---
+++ front end/bloco10-introdução a react/Dia 10.1 - 'Hello, world' no React/nome-app/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyName extends React.Component{
+    render (){
+        return <div>
+        <h1>Raphael Sant'Ana</h1>
+        <p>Vai Corinthians!</p>
+        </div>
+    }
+}
+
+export default MyName;
---
+++ front end/bloco10-introdução a react/Dia 10.1 - 'Hello, world' no React/nome-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyName from './About';
+
+class App extends React.Component{
+  render() {
+   return < MyName/>
+  }
+}
+
+export default App;
