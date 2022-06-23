0ce9d5e33b04591433d1aca51184669ea0fe6781
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-06-23 20:13:28 -0300

    Transmitindo informações de componente filho para componente pai

--- front end/bloco11-componetes com estado, eventos e formularios com react/Dia 11.2 - Formulários no React/exercicio-11.2-app/src/Forms.jsx
+++ front end/bloco11-componetes com estado, eventos e formularios com react/Dia 11.2 - Formulários no React/exercicio-11.2-app/src/Forms.jsx
@@ -1,110 +1,82 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
+import DataFieldset from './DataFieldset';
+import PersonalFieldset from './PersonalFieldset';
 
-  class Form extends Component {
-    constructor() {
-      super();
+class Form extends Component {
+  constructor() {
+    super();
 
-      this.state = {
-        name: '',
-        email: '',
-        age: '',
-        anecdote: '',
-        terms: false,
-      };
+    this.state = {
+      name: '',
+      email: '',
+      age: '',
+      anecdote: '',
+      terms: false,
+      formularioComErros: true,
+    };
 
-      this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange({ target }) {
-        const { name } = target;
-        const value = (target.type === 'checkbox') ? target.checked : target.value;
-
-      this.setState({
-        [name]: value,
-      });
-    }
-
-    renderSelecinput(){
-        const { age } = this.state;
-        return (
-            <label htmlFor="age">
-            Idade:
-            <select
-              id="age"
-              name="age"
-              onChange={ this.handleChange }
-              value={ age }
-            >
-              <option value="">Selecione</option>
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
-
-        )
-    }
-
-    render() {
-      const { name, email, anecdote, terms } = this.state;
-
-      return (
-        <div>
-          <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-          <form className="form">
-            <fieldset>
-            <legend>Informações pessoais</legend>
-            <label htmlFor="name">
-              Nome:
-              <input
-                id="name"
-                name="name"
-                type="text"
-                onChange={ this.handleChange }
-                value={ name }
-              />
-            </label>
-
-            <label htmlFor="email">
-              Email:
-              <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={ this.handleChange }
-                value={ email }
-              />
-            </label>
-
-        { this.renderSelecinput() }
-          </fieldset>
-
-            <fieldset>
-                <legend>Texto e arquivos</legend>
-            <label htmlFor="anecdote">
-              Anedota:
-              <textarea
-               id="anecdote"
-               name="anecdote" 
-               onChange={ this.handleChange }
-               value={ anecdote }
-              />
-            </label>
-
-            <label htmlFor="terms"
-            id="terms"
-            type="checkbox"
-            name="terms"
-            onChange={this.handleChange}
-            value={ terms }
-            >
-            Concordo com termos e acordos
-            </label>
-            <input type="file" />
-            </fieldset>
-          </form>
-        </div>
-      );
-    }
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  export default Form;
+  handleError() {
+    const { name, email, age, anecdote, terms } = this.state;
+
+    const errorCases = [
+      !name.length,
+      !email.match(/^\S+@\S+$/i),
+      !age.length,
+      !anecdote.length,
+      !terms,
+    ];
+
+    const formularioPreenchido = errorCases.every((error) => error !== true);
+
+    this.setState({
+      formularioComErros: !formularioPreenchido,
+    });
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => { this.handleError(); });
+  }
+
+  render() {
+    const { name, email, age, anecdote, terms, formularioComErros } = this.state;
+
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form className="form">
+          <PersonalFieldset
+            nameValue={ name }
+            emailValue={ email }
+            ageValue={ age }
+            handleChange={ this.handleChange }
+          />
+
+          <DataFieldset anecdoteValue={ anecdote } handleChange={ this.handleChange } />
+
+          <label htmlFor="terms">
+            <input
+              id="terms"
+              type="checkbox"
+              name="terms"
+              onChange={ this.handleChange }
+              value={ terms }
+            />
+            Concordo com termos e acordos
+          </label>
+        </form>
+        { formularioComErros
+          ? <span style={ { color: 'red' } }>Preencha todos os campos</span>
+          : <span style={ { color: 'green' } }>Todos campos foram preenchidos</span> }
+      </div>
+    );
+  }
+}
+
+export default Form;
