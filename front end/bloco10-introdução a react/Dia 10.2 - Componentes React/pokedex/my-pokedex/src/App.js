ad752a1ab7ce64c6c41fe062a5b3acd4af030596
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-06-24 09:15:29 -0300

    exercício - pokedex

---
+++ front end/bloco10-introdução a react/Dia 10.2 - Componentes React/pokedex/my-pokedex/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+class App extends React.Component {
+render() {
+  return (
+    <div className="App">
+      <h1> Pokedex </h1>
+      <Pokedex pokemons={pokemons} />
+    
+    </div>
+  );
+}
+
+}
+
+export default App;
