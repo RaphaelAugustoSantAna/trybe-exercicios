5f8d6846ff77b0fe59a42bc427c80bccabd5a2fc
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-07-15 08:45:14 -0300

    exercicios mock e input- 2 e 3

--- front end/bloco14-testes automatizados com react testing library/dia-2-rtl-mocks-e-inputs/service.test.js
+++ front end/bloco14-testes automatizados com react testing library/dia-2-rtl-mocks-e-inputs/service.test.js
@@ -1,9 +1,26 @@
 const service = require('./service');
 
+describe('testando as implementações',() => {
 test('Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
-    service.numRandom = jest.fn().mockReturnValue(10);
+    service.numRandom = jest.fn().mockImplementationOnce((a, b, c) => a * b * c);
 
-    expect(service.numRandom()).toBe(10);
+    expect(service.numRandom(2, 2, 2)).toBe(8);
     expect(service.numRandom).toHaveBeenCalled();
     expect(service.numRandom).toHaveBeenCalledTimes(1);
+    expect(service.numRandom).toHaveBeenCalledWith(2, 2, 2);
+
+});
+test('desenvolva uma nova implementação que receba três parâmetros', () => {
+    service.numRandom.mockReset();
+    expect(service.numRandom).toHaveBeenCalledTimes(0);
+
+    service.numRandom = jest.fn().mockImplementationOnce(a => a * 2);
+
+    expect(service.numRandom(6)).toBe(12);
+    expect(service.numRandom).toHaveBeenCalled();
+    expect(service.numRandom).toHaveBeenCalledTimes(1);
+    expect(service.numRandom).toHaveBeenCalledWith(6);
+});
+
+
 });
