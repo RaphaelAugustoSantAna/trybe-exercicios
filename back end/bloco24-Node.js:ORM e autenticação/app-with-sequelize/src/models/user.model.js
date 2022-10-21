cdfd6d9e4b61ebb1370fa82125eb9020bf4c4a43
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-10-21 09:57:55 -0300

    exercicios dia- 1 seção 6-Node.js:ORM e Autenticação

--- back end/bloco24-Node.js:ORM e autenticação/app-with-sequelize/src/models/user.model.js
+++ back end/bloco24-Node.js:ORM e autenticação/app-with-sequelize/src/models/user.model.js
@@ -1,11 +1,10 @@
 const UserModel = (sequelize, DataTypes) => {
-    const user = sequelize.define('User', {
-        fullName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        phoneNum: DataTypes.STRING,
+    const User = sequelize.define('User', {
+      fullName: DataTypes.STRING,
+      email: DataTypes.STRING,
     });
-
+  
     return User;
-}
-
-module.exports = UserModel;
+  };
+  
+  module.exports = UserModel;
---
+++ back end/bloco24-Node.js:ORM e autenticação/app-with-sequelize/src/seeders/20221021121503-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', 
+  [
+    {
+      fullName: 'Leonardo',
+      email: 'leo@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    {
+      fullName: 'JEduardo',
+      email: 'edu@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+
+  ], {}),
+  
+
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
+   
+};
