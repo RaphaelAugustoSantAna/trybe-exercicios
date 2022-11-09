c30187110442581dc698878583fb644d6a231cad
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2022-11-09 15:50:06 -0300

    exercicios de fixação

---
+++ back end/bloco27-programação orientada a objetos(POO) e SOLID/dia - 1 - introdução a POO/Exercicios de fixação/exemploCourse.ts
@@ -0,0 +1,42 @@
+// sintase geral
+
+// Criação da classe
+class Person {
+  // Atributos 
+    name: string;
+    height: number;
+    weight: number;
+  
+    // Métodos
+    // Método constructor
+    constructor(n: string, h: number, w: number) {
+      console.log(`Creating person ${n}`);
+      this.name = n; // Atribui valores de fora aos atributos do objeto
+      this.height = h;
+      this.weight = w;
+    }
+  
+    sleep() {
+      console.log(`${this.name}: zzzzzzz`);
+    }
+  }
+  
+  // Criação dos objetos
+  const p1 = new Person('Maria', 171, 58);
+  const p2 = new Person('João', 175, 66);
+  console.log(p1.name, p1.height, p1.weight);
+  console.log(p2.name, p2.height, p2.weight);
+
+  // Acesso aos metodos
+  p1.sleep();
+  p2.sleep();
+  
+  /*
+  Saída:
+  Creating person Maria
+  Creating person João
+  Maria 171 58
+  João 175 66
+  Maria: zzzzzzz
+  João: zzzzzzz
+  */
