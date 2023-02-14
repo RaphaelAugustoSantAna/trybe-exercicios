d9a21561ac34bb876292f3fed72e152832337ff9
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2023-02-14 15:24:02 -0300

    exercicio de fixação

--- ciencia da computação/seção1.1-introdução a Python/exercicios_de_fixação/tipos_de_dados.py
+++ ciencia da computação/seção1.1-introdução a Python/exercicios_de_fixação/tipos_de_dados.py
@@ -177,3 +177,43 @@ count_elements = {
     2: 2
 }
 
+# Exercício 12
+# O Fatorial de um número inteiro é representado pela multiplicação 
+# de todos os números predecessores maiores que 0. 
+# Por exemplo, o fatorial de 5 é 120 pois 5! = 1 * 2 * 3 * 4 * 5. 
+# Escreva um código que calcule o fatorial de um número inteiro.
+
+number = 5
+counter = 1
+result = 1
+while counter <= number:
+    result = result * counter
+    counter = counter + 1
+    print(result)
+
+
+# Exercício 13
+# Um sistema de avaliações registra valores de 0 a 10 para cada avaliação. 
+# Após algumas mudanças estes valores precisam ser ajustados 
+# para avaliações de 0 a 100. 
+# Dado uma sequência de avaliações ratings = [6, 8, 5, 9, 10].
+# Escreva um código capaz de gerar as avaliações após a mudança. 
+# Neste caso o resultado deveria ser [60, 80, 50, 90, 100].
+
+ratings = [6, 8, 5, 9, 10]
+new_ratings = []
+
+for rating in ratings:
+  new_ratings.append(rating * 10)
+
+new_ratings
+
+# Exercício 14
+# Percorra a lista do exercício 13 e imprima “Múltiplo de 3” 
+# se o elemento for divisível por 3.
+
+ratings = [6, 8, 5, 9, 10]
+
+for rating in ratings:
+    if (rating % 3) == 0:
+        print(f'{rating} é multiplo de 3')
