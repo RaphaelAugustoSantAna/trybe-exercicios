7da403c23257e52c7c608f532405c90c0131cc2f
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2023-03-01 10:26:31 -0300

    Herança

--- ciencia da computação/seção02-programação orientada a objetos e padrões de projetos/Dia 01: P.O.O em Python/exemplos/exemplos_course.py
+++ ciencia da computação/seção02-programação orientada a objetos e padrões de projetos/Dia 01: P.O.O em Python/exemplos/exemplos_course.py
@@ -134,3 +134,62 @@ class Pessoa:
 
 print(pessoa_cozinheira)
 # retorno: Jacquin - possui 800 reais em sua conta.
+
+class Eletrodomestico:
+    def __init__(self, cor, potencia, tensao, preco):
+        self.preco = preco
+        self.cor = cor
+        self._potencia = potencia
+        self._tensao = tensao
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+    @property
+    def cor(self):
+        return self.__cor.upper()
+
+    @cor.setter
+    def cor(self, nova_cor):
+        self.__cor = nova_cor
+
+# Um método pode chamar um outro método já declarado
+#  na superclasse da seguinte maneira:
+
+class A:
+    def faz_algo(self, valor):
+        print(valor)
+
+
+class B(A):
+    def faz_outra_coisa(self):
+        print("Vou printar o valor pelo método criado em A")
+        # Chama o método da classe A, que neste caso é a superclasse, passando
+        # o `self` explicitamente
+        A.faz_algo(self, valor=1)
+
+
+b = B()
+b.faz_outra_coisa()
+# Vou printar o valor pelo método criado em A
+# 1
