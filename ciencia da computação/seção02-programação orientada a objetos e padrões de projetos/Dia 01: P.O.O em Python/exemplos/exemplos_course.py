79a8521e7781ffb091a2cd63e4421923801a8f31
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2023-02-28 22:30:45 -0300

    Encapsulamento e Abstração

--- ciencia da computação/seção02-programação orientada a objetos e padrões de projetos/Dia 01: P.O.O em Python/exemplos/exemplos_course.py
+++ ciencia da computação/seção02-programação orientada a objetos e padrões de projetos/Dia 01: P.O.O em Python/exemplos/exemplos_course.py
@@ -18,3 +18,30 @@ class Liquidificador:
         # Perceba que é possível ter atributos que não precisam ser passados por meio de parâmetros 
         # na chamada do construtor. Por exemplo, para o booleano __ligado e o inteiro __velocidade, 
         # o construtor vai iniciá-los sempre com os valores fixos (hard coded) False e 0, respectivamente.
+    
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+        
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+    
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+    
+    def esta_ligado(self):
+        return self.__ligado
+        
+liquidificador_vermelho = Liquidificador("Vermelho", 250, 220, 100)
+liquidificador_vermelho.ligar(1)
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? True
+liquidificador_vermelho.desligar()
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? False
