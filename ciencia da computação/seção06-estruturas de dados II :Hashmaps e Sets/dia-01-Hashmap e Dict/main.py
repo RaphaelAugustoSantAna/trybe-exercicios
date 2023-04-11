99d689c3ed92ef357bef13d52fd72ee016d16411
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2023-04-11 14:53:43 -0300

    resolução de problema:encontrar o número com maior frequência

---
+++ ciencia da computação/seção06-estruturas de dados II :Hashmaps e Sets/dia-01-Hashmap e Dict/main.py
@@ -0,0 +1,4 @@
+from resolucao_de_problema import count
+
+nums = [3,2,5,4,1,2,3,1,3,4,1]
+print(count(nums))
---
+++ ciencia da computação/seção06-estruturas de dados II :Hashmaps e Sets/dia-01-Hashmap e Dict/resolucao_de_problema.py
@@ -0,0 +1,14 @@
+def count(nums):
+    count = {}
+    most_frequent = nums[0]
+
+    for num in nums:
+        if num not in count:
+            count[num] = 1
+        else:
+            count[num] += 1
+
+        if count[num] > count[most_frequent]:
+            most_frequent = num
+
+    return most_frequent
